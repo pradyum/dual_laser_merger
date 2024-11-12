6c597d3310e4f2884705afe9b2793d30f45ee017
pradyum <pradyum.30@gmail.com>
2024-11-12 08:16:52 +0530

    fixed conflict

--- src/dual_laser_merger.cpp
+++ src/dual_laser_merger.cpp
@@ -29,29 +29,18 @@ MergerNode::MergerNode(const rclcpp::NodeOptions & options)
     RCLCPP_INFO(this->get_logger(), "Target Frame: %s", target_frame_param.c_str());
   }
 
-<<<<<<< HEAD
-  merged_pub =
-    this->create_publisher<sensor_msgs::msg::LaserScan>(
-    this->get_parameter(
-      "merged_topic").as_string(), rclcpp::SensorDataQoS());
-  laser_1_sub.subscribe(
-    this, this->get_parameter("laser_1_topic").as_string(),
-    rclcpp::SensorDataQoS().get_rmw_qos_profile());
-  laser_2_sub.subscribe(
-    this, this->get_parameter("laser_2_topic").as_string(),
-    rclcpp::SensorDataQoS().get_rmw_qos_profile());
-=======
   merged_scan_pub =
     this->create_publisher<sensor_msgs::msg::LaserScan>(this->get_parameter(
       "merged_scan_topic").as_string(), rclcpp::SensorDataQoS());
   merged_cloud_pub =
     this->create_publisher<sensor_msgs::msg::PointCloud2>(this->get_parameter(
       "merged_cloud_topic").as_string(), rclcpp::SensorDataQoS());
-  laser_1_sub.subscribe(this, this->get_parameter("laser_1_topic").as_string(),
-      rclcpp::SensorDataQoS());
-  laser_2_sub.subscribe(this, this->get_parameter("laser_2_topic").as_string(),
-      rclcpp::SensorDataQoS());
->>>>>>> 208df64 (added shadow and average filters)
+  laser_1_sub.subscribe(
+    this, this->get_parameter("laser_1_topic").as_string(),
+    rclcpp::SensorDataQoS().get_rmw_qos_profile());
+  laser_2_sub.subscribe(
+    this, this->get_parameter("laser_2_topic").as_string(),
+    rclcpp::SensorDataQoS().get_rmw_qos_profile());
 
   tf2_buffer = std::make_shared<tf2_ros::Buffer>(this->get_clock());
   tf2_listener = std::make_shared<tf2_ros::TransformListener>(*tf2_buffer, this);
